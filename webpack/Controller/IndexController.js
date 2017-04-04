a9f086eb25cba8b240aceb59b6b65df74d2761a3
Rimpei Kunimoto <ht14a022@oecu.jp>
2017-04-05 01:59:10 +0900

    サービス叩いてみた　すごい

--- webpack/Controller/IndexController.js
+++ webpack/Controller/IndexController.js
@@ -1,10 +1,12 @@
 "use strict";
 
 export default class IndexController {
-    constructor($location, $http, $state) {
+    constructor($location, $http, $state, Bbslog) {
         "ngInject";
         this.$http = $http;
         this.$state = $state;
         this.text = "Hello";
+
+        this.list = Bbslog.find();
     }
 }
